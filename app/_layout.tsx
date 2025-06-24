c034d9e1e609cbe66e5f8f8659fd083497002b92
kishlay <kishlay141@gmail.com>
2025-06-24 11:18:20 +0530

    updated the _laout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,23 @@
 import { Stack } from "expo-router";
 import { useColorScheme } from "react-native";
-import { PaperProvider } from "react-native-paper";
+import { MD3DarkTheme, MD3LightTheme, PaperProvider } from "react-native-paper";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
+  const theme = colorScheme === "dark" ? MD3DarkTheme : MD3LightTheme;
+
+  const customTheme = {
+    ...theme,
+    colors: {
+      ...theme.colors,
+      primary: "#6200ee",
+      background: colorScheme === "dark" ? "#121212" : "#f5f5f5",
+    },
+  };
+
   return (
-    <PaperProvider
-      theme={{
-        mode: colorScheme || "light",
-        colors: {
-          primary: "#6200ee",
-          background: colorScheme === "dark" ? "#121212" : "#f5f5f5",
-        },
-      }}
-    >
+    <PaperProvider theme={customTheme}>
       <Stack>
         <Stack.Screen name="pin" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
